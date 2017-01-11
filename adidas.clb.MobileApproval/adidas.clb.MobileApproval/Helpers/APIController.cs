40e787d6c8538e6945ca57163a9d0a00d2cd2c9e
thotaram <thotaram@b058d400-cee8-4732-908c-c1ffa6c0338c>
2017-01-11 08:46:15 +0000

    Added retry/backup logic to approval API

--- adidas.clb.MobileApproval/adidas.clb.MobileApproval/Helpers/APIController.cs
+++ adidas.clb.MobileApproval/adidas.clb.MobileApproval/Helpers/APIController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using adidas.clb.MobileApproval.Utility;
 using adidas.clb.MobileApproval.Exceptions;
@@ -35,34 +37,65 @@ namespace adidas.clb.MobileApproval.Helpers
                 //get API endpoint and format it in agents/{backendID}/requestApproval/{reqID}
                 string backendApiEndpoint = UrlSettings.GetBackendAgentRequestApprovalAPI(backendID, apprReqID);
                 string approvalquery = JsonConvert.SerializeObject(apprReqDetails);
+                //Max Retry call from web.config
+                int maxRetryCount = Convert.ToInt32(ConfigurationManager.AppSettings["MaxRetryCount"]);
+                int maxThreadSleepInMilliSeconds = Convert.ToInt32(ConfigurationManager.AppSettings["MaxThreadSleepInMilliSeconds"]);
+                int RetryAttemptCount = 0;
+                bool IsSuccessful = false;
                 //checking backend endpoint null or empty
                 if (!string.IsNullOrEmpty(backendApiEndpoint))
                 {
                     using (HttpClient client = new HttpClient())
                     {
-                        //POST: Submits the approval or rejection for one specific request
-                        var request = new HttpRequestMessage(HttpMethod.Post, backendApiEndpoint);
-                        request.Content = new StringContent(approvalquery, Encoding.UTF8, "application/json");
-                        var resultset = client.SendAsync(request).Result;                        
-                        //if response message returns success code then return the successcode message
-                        if (resultset.IsSuccessStatusCode)
+                        //Implemented Use of retry / back-off logic
+                        do
                         {
-                            string response = resultset.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                            if (!string.IsNullOrEmpty(response))
+                            try
                             {
-                                //request update acknowledgement
-                                RequestsUpdateAck Objacknowledgement = JsonConvert.DeserializeObject<RequestsUpdateAck>(response);
-                                //if request update acknowledgement error object is null means backend api successfully called
-                                if (Objacknowledgement.Error == null)
+                                //POST: Submits the approval or rejection for one specific request
+                                var request = new HttpRequestMessage(HttpMethod.Post, backendApiEndpoint);
+                                request.Content = new StringContent(approvalquery, Encoding.UTF8, "application/json");
+                                var resultset = client.SendAsync(request).Result;
+                                //if response message returns success code then return the successcode message
+                                if (resultset.IsSuccessStatusCode)
                                 {
-                                    result = "OK";
-                                }                               
+                                    string response = resultset.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                                    if (!string.IsNullOrEmpty(response))
+                                    {
+                                        //request update acknowledgement
+                                        RequestsUpdateAck Objacknowledgement = JsonConvert.DeserializeObject<RequestsUpdateAck>(response);
+                                        //if request update acknowledgement error object is null means backend api successfully called
+                                        if (Objacknowledgement.Error == null)
+                                        {
+                                            result = "OK";
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    result = "Error occurred while saving data";
+                                }
+                                IsSuccessful = true;
                             }
-                        }
-                        else
-                        {
-                            result = "Error occurred while saving data";
-                        }
+                            catch (Exception serviceException)
+                            {
+                                //Increasing RetryAttemptCount variable
+                                RetryAttemptCount = RetryAttemptCount + 1;
+                                //Checking retry call count is eual to max retry count or not
+                                if (RetryAttemptCount == maxRetryCount)
+                                {
+                                    LoggerHelper.WriteToLog(serviceException + " - exception while Call baceknd agent request approval API in  ApprovalAPIController::Retry attempt count: [" + RetryAttemptCount + "]" + serviceException.ToString(), CoreConstants.Priority.High, CoreConstants.Category.Error);
+                                    throw new ServiceLayerException(serviceException.Message, serviceException.InnerException);
+                                }
+                                else
+                                {
+                                    LoggerHelper.WriteToLog(serviceException + " - exception while Call baceknd agent request approval API in  ApprovalAPIController::Retry attempt count: [" + RetryAttemptCount + "]" + serviceException.ToString(), CoreConstants.Priority.High, CoreConstants.Category.Error);
+                                    //Putting the thread into some milliseconds sleep  and again call the same method call.
+                                    Thread.Sleep(maxThreadSleepInMilliSeconds);
+                                }
+                            }
+                        } while (!IsSuccessful);
+                       
                     }
                     return result;
                 }
