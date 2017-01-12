eb940da49d3ed197a0a6fdc632435664fc0daac9
thotaram <thotaram@b058d400-cee8-4732-908c-c1ffa6c0338c>
2017-01-12 05:19:37 +0000

    Added retry / back-off logic

--- adidas.clb.MobileApproval/adidas.clb.job.GeneratePDF/Helpers/AzureQueues.cs
+++ adidas.clb.MobileApproval/adidas.clb.job.GeneratePDF/Helpers/AzureQueues.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Configuration;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
 using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.WindowsAzure.Storage.Queue;
 using adidas.clb.job.GeneratePDF.Exceptions;
@@ -31,6 +32,11 @@ namespace adidas.clb.job.GeneratePDF.Helpers
                 CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["UpdateTriggerAzureQueues"]);
                 // Create the queue client.
                 CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+                // set retry for the connection for transient failures
+                queueClient.DefaultRequestOptions = new QueueRequestOptions
+                {
+                    RetryPolicy = new LinearRetry(TimeSpan.FromSeconds(5), 3)
+                };
                 return queueClient;
             }
             catch (Exception exception)
@@ -40,27 +46,7 @@ namespace adidas.clb.job.GeneratePDF.Helpers
             }
 
         }
-        /// <summary>
-        /// This method Retrieves a reference to a Update Trigger Input Queue.
-        /// </summary>
-        /// <param name="queuePath"></param>
-        /// <returns></returns>
-        public static CloudQueue GetInputQueue(CloudQueueClient queuePath)
-        {
-            string callerMethodName = string.Empty;
-            try
-            {
-                //Get Caller Method name from CallerInformation class
-                callerMethodName = CallerInformation.TrackCallerMethodName();
-                //read queue name from app.config :: AppSettings
-                return queuePath.GetQueueReference(ConfigurationManager.AppSettings["UpdateTriggerInputQueue"]);
-            }
-            catch (Exception exception)
-            {
-                InsightLogger.Exception(exception.Message, exception, callerMethodName);
-                throw new DataAccessException(exception.Message, exception.InnerException);
-            }
-        }
+            
         /// <summary>
         /// This method Retrieves a reference to a Request PDF Queue.
         /// </summary>
