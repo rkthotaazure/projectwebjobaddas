1e2013a33635f25a26170c9f77678156e51472ba
mannesuk <mannesuk@b058d400-cee8-4732-908c-c1ffa6c0338c>
2017-04-19 12:45:08 +0000

    integrated approver comments changes

--- adidas.clb.MobileApproval/adidas.clb.MobileApprovalUI/Models/JSONHelper/ServiceObject.cs
+++ adidas.clb.MobileApproval/adidas.clb.MobileApprovalUI/Models/JSONHelper/ServiceObject.cs
@@ -144,6 +144,7 @@ namespace adidas.clb.MobileApprovalUI.Models.JSONHelper
         public string Status { get; set; }
         public DateTime? DueDate { get; set; }
         public DateTime? DecisionDate { get; set; }
+        public string Comment { get; set; }
     }
 
     public class UserDeviceJsonData
